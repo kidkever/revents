6a404cbb3de90276d2fb951a250190ca0aa17eb6
Mazen Mohamed <kidkever@yahoo.com>
2021-01-30 08:31:49 +0200

    redux thunk and async code

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -8,6 +8,7 @@ import EventDetailedPage from "../../features/events/eventDetailed/EventDetailed
 import NavBar from "../../features/nav/NavBar";
 import Sandbox from "../../features/sandbox/Sandbox";
 import ModalManager from "../common/modals/ModalManager";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   const { key } = useLocation();
@@ -15,6 +16,7 @@ const App = () => {
   return (
     <>
       <ModalManager />
+      <ToastContainer position="bottom-right" />
       <Route path="/" component={HomePage} exact />
       <Route
         path={"/(.+)"}
